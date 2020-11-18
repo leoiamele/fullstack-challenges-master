483b16c7d2d25a12bdb1a4707d11c503c5cb255a
Leandro Iamele <leandroiamele@gmail.com>
2020-11-17 23:26:33 -0300

    Age in days method with interface

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/interface.rb
@@ -6,18 +6,20 @@ require_relative './age_in_days'
 ### Talking with the user ###
 puts 'What\'s your year of birth?'
 birth_year = gets.chomp.to_i
+puts birth_year
 
 puts 'What\'s your month of birth?'
 birth_month = gets.chomp.to_i
-
+puts birth_month
 puts 'What\'s your day of birth?'
 birth_day = gets.chomp.to_i
+puts birth_day
 #############################
 
 puts 'Computing your age (with the most complicated algorithms)........'
 
 # TODO: This is probably where you'd like to use your brand new function!
-calculated_age = 0
+calculated_age = age_in_days(birth_day, birth_month, birth_year)
 
 
 # Finally, print user's age in days:
