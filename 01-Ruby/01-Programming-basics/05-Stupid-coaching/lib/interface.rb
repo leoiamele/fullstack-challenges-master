d88c3fd792820023cf03b31ee1c1ddb361a47a90
Leandro Iamele <leandroiamele@gmail.com>
2020-11-19 10:36:57 -0300

    stupid coach part one

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,12 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+loop do
+  puts "How can I help you my kind student?"
+  your_message = gets.chomp
+  answer = coach_answer(your_message)
+  if answer == ""
+    break
+  else
+    puts answer
+  end
+ end
